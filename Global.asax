306cf87370eb55834e75c966bd505cfd6c74ca69
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2022-02-11 14:28:45 +0100

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP220211V3.MvcApplication" Language="C#" %>
---
+++ Models/ElofizetesTipus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ASP220211V3.Models
+{
+    [Table("ElofizetesTipusok")]
+    public class ElofizetesTipus
+    {
+        [Required, Key]
+        public int EloofizetesTipusId { get; set; }
+        [Required, StringLength(10), Column(TypeName = "VARCHAR")]
+        public string Nev { get; set; }
+        public int Ar { get; set; }
+        public int HonapokSzama { get; set; }
+        public int TermekKedvezmeny { get; set; }
+    }
+}
